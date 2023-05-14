182d3c079dc381f312e364910cb8dd7c9140bf7c
manubrizu <84647931+manubrizu@users.noreply.github.com>
2023-05-13 21:05:53 -0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,14 +1,11 @@
 import qrcode
-import image
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=4,
+)
+qr.add_data('algo')
+qr.make(fit=True)
 
-qr = qrcode.QRCode(version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=12,
-    border=2)
-data = "http://165.227.202.250/login/index.php"
-qr.add_data(data)
-qr.make(fit =True)
-img = qr.make_image(fill = 'Black',back_color = 'White')
-img.save("image.png")
-from IPython.display import display, Image 
-display(Image(filename='image.png'))
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr-ml.png")
